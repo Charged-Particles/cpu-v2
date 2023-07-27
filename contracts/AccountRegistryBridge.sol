4a49db1c142afa3a4bc45b13e7aa258b185be3a0
scammi <scammi@gmail.com>
2023-07-27 10:03:29 -0300

    Pass down to AccountRegistryBridge the implementation address.

--- contracts/AccountRegistryBridge.sol
+++ contracts/AccountRegistryBridge.sol
@@ -5,14 +5,14 @@ pragma solidity ^0.8.13;
 import "./interfaces/IRegistry.sol";
 contract AccountRegistryBridge {
     address public constant REGISTRY = 	0x02101dfB77FDE026414827Fdc604ddAF224F0921;
-    address public constant IMPLMENTATION = 0x2D25602551487C3f3354dD80D76D54383A243358;
 
-    function createAccount(address contractAddress, uint256 tokenId)
-        external
-        returns (address)
-    {
+    function createAccount(
+        address implmentation,
+        address contractAddress,
+        uint256 tokenId
+    ) external returns (address) {
         return IRegistry(REGISTRY).createAccount(
-            IMPLMENTATION,
+            implmentation,
             block.chainid,
             contractAddress,
             tokenId,
@@ -21,13 +21,13 @@ contract AccountRegistryBridge {
         );
     }
 
-    function account(address contractAddress, uint256 tokenId)
-        external
-        view
-        returns (address)
-    {
+    function account(
+        address implmentation,
+        address contractAddress,
+        uint256 tokenId
+    ) external view returns (address) {
         return IRegistry(REGISTRY).account(
-            IMPLMENTATION,
+            implmentation,
             block.chainid,
             contractAddress,
             tokenId,
