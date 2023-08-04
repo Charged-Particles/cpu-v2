3ce672e83abcbd9f413d38714e95fce0b61d287b
scammi <scammi@gmail.com>
2023-08-04 20:05:20 -0300

    test deployment of ionx

---
+++ test/Ionx.test.ts
@@ -0,0 +1,23 @@
+import { expect } from "chai";
+import { ethers, network, getNamedAccounts, deployments } from 'hardhat';
+import { Ionx } from "../typechain-types";
+
+describe('Ionx deployment', async () => {
+  let ionx: Ionx;
+  let deployer: string;
+
+  beforeEach(async () => {
+    await deployments.fixture(['Ionx']);
+    ionx = await ethers.getContract('Ionx');
+  });
+
+  before(async () => {
+    const { deployer: deployerAccount, user1 } = await getNamedAccounts();
+    deployer = deployerAccount;
+  });
+
+  it ('Deployer is rich $$', async () => {
+    const deployerBalance = await ionx.balanceOf(deployer);
+    expect(deployerBalance).to.greaterThan(1000n)
+  });
+});
