af66d8f1156842474d7a5be67993931a87d87ef6
scammi <scammi@gmail.com>
2023-08-05 16:32:44 -0300

    bonds

--- test/RewardProgramSetupTestnet.test.ts
+++ test/RewardProgramSetupTestnet.test.ts
@@ -1,25 +1,58 @@
 import { expect } from "chai";
 import { ethers, network, getNamedAccounts, deployments } from 'hardhat';
 import { getChargedParticlesOwner } from "../utils/getSigners";
+import { ChargedParticles, ChargedSettings, Lepton2 } from "../typechain-types";
+import { addressBook } from "../utils/globals";
+import { Signer } from "ethers";
 
 describe('RewardProgramSetupTestnet deployments', async () => {
-  let deployer, user, chargedOwner;
+  let chargedParticles: ChargedParticles, chargedSettings: ChargedSettings;
+  let lepton: Lepton2;
 
-  beforeEach(async () => {
-    await deployments.fixture(['RPSetupTest']);
-  });
+  let deployer: string, user: string, chargedOwner: Signer;
+  let leptonAddress: string, chargedParticlesAddress: string;
+  let chainId;
 
   before(async () => {
     const { deployer: deployerAccount, user1 } = await getNamedAccounts();
-    chargedOwner = getChargedParticlesOwner();
+    chargedOwner = await getChargedParticlesOwner();
     deployer = deployerAccount;
     user = user1;
   });
 
-  it ('Energizes ', async () => {
-      
+  before(async () => {
+    await deployments.fixture(['RPSetupTest']);
+
+    chainId = network.config.chainId ?? 1;
+    lepton = await ethers.getContract('Lepton2');
+    chargedParticles = await ethers.getContractAt('ChargedParticles', addressBook[chainId].chargedParticles, chargedOwner);
+    chargedSettings = await ethers.getContractAt('ChargedSettings', addressBook[chainId].chargedSettings, chargedOwner);
+
+    leptonAddress = await lepton.getAddress();
+    chargedParticlesAddress = await chargedParticles.getAddress();
+
+    chargedSettings.enableNftContracts([ leptonAddress ]).then(tx => tx.wait());
   });
 
   it ('Bonds', async () => {
+    await lepton.mintLepton({ value: ethers.parseEther('0.3') }).then(tx => tx.wait());
+    await lepton.mintLepton({ value: ethers.parseEther('0.3') }).then(tx => tx.wait());
+    expect(await lepton.balanceOf(deployer)).to.be.eq(2);
+
+    await lepton.approve(chargedParticlesAddress, 2).then(tx => tx.wait());
+
+    await chargedParticles.connect(await ethers.getSigner(deployer)).covalentBond(
+      leptonAddress,
+      1,
+      'generic.B',
+      leptonAddress,
+      2,
+      1
+    ).then(tx => tx.wait());
   });
+
+  it ('Energizes ', async () => {
+    
+  });
+  
 });
--- utils/globals.ts
+++ utils/globals.ts
@@ -3,20 +3,24 @@ interface AddressBook {
   [chainId: number]: {
     chargedManager: string;
     chargedParticles: string;
+    chargedSettings: string;
   };
 }
 
 export const addressBook: AddressBook = {
   1: {
     'chargedManager': '0x7b07Ec627d2426b89C44a6cC75Dc57c27a52174d',
-    'chargedParticles': '0xaB1a1410EA40930755C1330Cc0fB3367897C8c41'
+    'chargedParticles': '0xaB1a1410EA40930755C1330Cc0fB3367897C8c41',
+    'chargedSettings' : '0x07DdB208d52947320d07E0E4611a80Fb7eFD001D'
   },
   31337: {
     'chargedManager': '0x7b07Ec627d2426b89C44a6cC75Dc57c27a52174d',
-    'chargedParticles': '0xaB1a1410EA40930755C1330Cc0fB3367897C8c41'
+    'chargedParticles': '0xaB1a1410EA40930755C1330Cc0fB3367897C8c41',
+    'chargedSettings' : '0x07DdB208d52947320d07E0E4611a80Fb7eFD001D'
   },
   80001: {
     'chargedManager': '0xE8c6462ceEeeC3f8c318e29Af143f623de979D69',
-    'chargedParticles': '0x51f845af34c60499a1056FCDf47BcBC681A0fA39'
+    'chargedParticles': '0x51f845af34c60499a1056FCDf47BcBC681A0fA39',
+    'chargedSettings' : '0x60428D3e580907C74Ee8690E4E192317864aAE1d'
   }
 }
