a8aa78f4ed0ae7433a088a26d94945dc067c96be
scammi <scammi@gmail.com>
2023-09-19 14:26:10 -0300

    log

--- scripts/set_reward_program_multiplier.ts
+++ scripts/set_reward_program_multiplier.ts
@@ -5,7 +5,9 @@ async function main() {
   const multiplierContract = '0x71758a4822b0e7b5c21ef8f73c69e528be4882c7';
   const universe: UniverseRP = await ethers.getContract('UniverseRPPolygon');
 
+  console.log('... setting new multiplier');
   await universe.setMultiplierNft(multiplierContract).then(tx => tx.wait());
+  console.log('new address set');
 }
 
 main().catch((error) => {
