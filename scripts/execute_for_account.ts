d5ab6017c24455993b6311adc07287e035a15765
scammi <scammi@gmail.com>
2023-12-12 16:30:30 -0300

    Restore controller after running script

--- scripts/execute_for_account.ts
+++ scripts/execute_for_account.ts
@@ -3,11 +3,12 @@ import { ethers, getNamedAccounts } from "hardhat";
 async function main() {
   const { deployer } = await getNamedAccounts();
   const receiver = '0x03828b7129d49313B2cdc966e50369B75EC79A48';
+  const chargedParticlesAddress = '0x0288280Df6221E7e9f23c1BB398c820ae0Aa6c10';
 
   // const chargedParticles: ChargedParticles = await ethers.getContractAt('ChargedParticles', '0x51f845af34c60499a1056FCDf47BcBC681A0fA39');
   const manager = await ethers.getContractAt('AaveWalletManager', '0x54b32b288d7904D5d98Be1910975a80e45DA5e8d');
 
-  const setControllerTx = await manager.setController(deployer).then(tx => tx.wait());
+  const setControllerTxBefore = await manager.setController(deployer).then(tx => tx.wait());
 
   const dischargeInterface = (recipient: string, amount: BigInt) => {
     const ABI = ["function transfer(address recipient, uint256 amount)"];
@@ -27,6 +28,8 @@ async function main() {
     0,
     dischargeCallData
   ).then(tx => tx.wait());
+
+  const setControllerTxAfter = await manager.setController(chargedParticlesAddress).then(tx => tx.wait());
 
   console.log(executeTx)
 }
