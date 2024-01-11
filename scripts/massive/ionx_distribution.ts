6336ead01ecf62d98216c24c3cd005c38b191313
scammi <scammi@gmail.com>
2024-01-11 11:29:30 -0300

    Update for latest release

--- scripts/massive/ionx_distribution.ts
+++ scripts/massive/ionx_distribution.ts
@@ -11,25 +11,27 @@ const main = async () => {
   const outputFile = 'scripts/massive/zealy_rewards_01_out.csv';
 
   fs.createReadStream(inputFile)
-  .pipe(csv({ separator: ';' }))
+  .pipe(csv({ separator: ',' }))
   .on('data', (row) => {
     results.push(row)
   })
   .on('end', async () => {
+    const ionx: Ionx = await ethers.getContractAt('Ionx', '0x01b317bc5ed573faa112ef64dd029f407cecb155');
+    const balance = await ionx.balanceOf('0x48F54e595bf039CF30fa5F768c0b57EAC6508a06');
+    let balanceLeft = balance;
+
     for (const user of results){
-      if (user._1 == 'Zealy Name') continue;
+      if (user._1 == 'Address') continue;
 
-      const dollarReward = Number(user._3);
-      const ionxAmount = dollarReward / 0.022329;
+      balanceLeft = balanceLeft - ethers.parseUnits(user.Reward, 18);
 
-      user['ionxAmount'] = ionxAmount;
-      const ionx: Ionx = await ethers.getContractAt('Ionx', '0x01b317bc5ed573faa112ef64dd029f407cecb155');
+      const sendTx = await ionx.transfer(user.Address, ethers.parseUnits(user.Reward, 18), { gasPrice: ethers.parseUnits('50','gwei') }).then(tx => tx.wait());
+      user['hashURL'] = 'https://polygonscan.com/tx/' + sendTx?.hash
+      user['hash'] = sendTx?.hash;
 
-      const balance = await ionx.balanceOf('0x48F54e595bf039CF30fa5F768c0b57EAC6508a06');
-      // const sendTx = await ionx.transfer(user._4, ethers.parseEther(String(ionxAmount)), { gasPrice: ethers.parseUnits('200','gwei') }).then(tx => tx.wait());
-      // user['hashURL'] = 'https://polygonscan.com/tx/' + sendTx?.hash
-      // user['hash'] = sendTx?.hash;
       console.log(balance.toString());
+      console.log(balanceLeft.toString());
+      console.log(user);
     };
   });
 };
