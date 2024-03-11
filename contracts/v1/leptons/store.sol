d264d87cd47ea5c4dacf4431e4e25c423a147ea4
Rob Secord <robsecord@gmail.com>
2024-03-11 12:00:16 -0400

    Updates for IONX permit

--- contracts/v1/leptons/store.sol
+++ contracts/v1/leptons/store.sol
@@ -4,7 +4,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "../interfaces/ILepton.sol";
 
 /*
-    TODO: 
+    TODO:
         - Buy with Ionx method
         - wormhole prevention
 */
@@ -12,14 +12,24 @@ import "../interfaces/ILepton.sol";
 interface ILepsonsStore {
    function load(uint256 amount) external payable;
    function setLepton(address _lepton) external;
+   function setIonx(address _ionx) external;
+   function buyWithIonx(uint256 leptonAmount, uint256 deadline, uint8 v, bytes32 r, bytes32 s) external;
+}
+
+interface IIonx {
+  function permit(address owner, address spender, uint256 amount, uint256 deadline, uint8 v, bytes32 r, bytes32 s) external;
+  function transfer(address recipient, uint256 amount) external returns (bool);
 }
 
 contract LeptonsStore is ILepsonsStore, IERC721Receiver, Ownable  {
 
+    IIonx public ionx;
     ILepton public lepton;
+    uint256 ionxPerLepton;
 
-    constructor(address _lepton) public {
+    constructor(address _lepton, address _ionx) public {
         lepton = ILepton(_lepton);
+        ionx = IIonx(_ionx);
     }
 
     function load(uint256 amount) external payable override onlyOwner {
@@ -31,6 +41,33 @@ contract LeptonsStore is ILepsonsStore, IERC721Receiver, Ownable  {
     }
 
     function setLepton(address _lepton) external override onlyOwner {
+        require(_lepton != address(0), "Invalid address");
         lepton = ILepton(_lepton);
+    }
+
+    function setIonx(address _ionx) external override onlyOwner {
+        require(_ionx != address(0), "Invalid address");
+        ionx = IIonx(_ionx);
+    }
+
+    function setIonxPerLepton(uint256 ionxAmount) external override onlyOwner {
+        ionxPerLepton = ionxAmount;
+    }
+
+    function buyWithIonx(uint256 leptonAmount, uint256 deadline, uint8 v, bytes32 r, bytes32 s) external override {
+      uint256 ionxAmount = leptonAmount * ionxPerLepton;
+      require(ionx.balanceOf(msg.sender) >= ionxAmount, "Insufficient IONX balance");
+
+      // IONX Approval (requires signtaure)
+      ionx.permit(msg.sender, address(this), ionxAmount, deadline, v, r, s);
+
+      // Payment
+      ionx.transferFrom(msg.sender, address(this), ionxAmount);
+
+      // Transfer Lepton to Buyer
+      uint256 tokenId = 123; // TODO: Get Next Token ID
+      lepton.safeTransferFrom(address(this), msg.sender, tokenId);
+
+      // TODO: Emit purchase event
     }
 }
