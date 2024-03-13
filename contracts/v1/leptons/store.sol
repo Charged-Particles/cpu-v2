497fc135db24ed12268196827867fed64dbf41c5
scammi <scammi@gmail.com>
2024-03-13 09:01:00 -0300

    Add buyWithIonx method

--- contracts/v1/leptons/store.sol
+++ contracts/v1/leptons/store.sol
@@ -1,7 +1,8 @@
 pragma solidity 0.6.12;
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "../interfaces/ILepton.sol";
+import "../tokens/Ionx.sol";
+import "../tokens/Lepton2.sol";
 
 /*
     TODO: 
@@ -16,10 +17,13 @@ interface ILepsonsStore {
 
 contract LeptonsStore is ILepsonsStore, IERC721Receiver, Ownable  {
 
-    ILepton public lepton;
+    Lepton2 public lepton;
+    Ionx public ionx;
+
+    uint256 ionxPerLepton;
 
     constructor(address _lepton) public {
-        lepton = ILepton(_lepton);
+        lepton = Lepton2(_lepton);
     }
 
     function load(uint256 amount) external payable override onlyOwner {
@@ -31,6 +35,32 @@ contract LeptonsStore is ILepsonsStore, IERC721Receiver, Ownable  {
     }
 
     function setLepton(address _lepton) external override onlyOwner {
-        lepton = ILepton(_lepton);
+        lepton = Lepton2(_lepton);
+    }
+
+    function setIonx(address _ionx) external onlyOwner {
+        require(_ionx != address(0), "Invalid address");
+        ionx = Ionx(_ionx);
+    }
+
+    function setIonxPerLepton(uint256 ionxAmount) external onlyOwner {
+        ionxPerLepton = ionxAmount;
+    }
+
+    function buyWithIonx(uint256 leptonAmount, uint256 deadline, uint8 v, bytes32 r, bytes32 s) external {
+      uint256 ionxAmount = leptonAmount * ionxPerLepton;
+      require(ionx.balanceOf(msg.sender) >= ionxAmount, "Insufficient IONX balance");
+
+      // IONX Approval (requires signtaure)
+      ionx.permit(msg.sender, address(this), ionxAmount, deadline, v, r, s);
+
+      // Payment
+      ionx.transferFrom(msg.sender, address(this), ionxAmount);
+
+      // Transfer Lepton to Buyer
+      uint256 tokenId = 123; // TODO: Get Next Token ID
+      lepton.safeTransferFrom(address(this), msg.sender, tokenId);
+
+      // TODO: Emit purchase event
     }
 }
