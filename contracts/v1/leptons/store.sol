bf705d9ebfcc2c9c03791c513ec1ff3e34e05710
Rob Secord <robsecord@gmail.com>
2024-03-21 12:42:06 -0400

    Cosmetic changes to source

--- contracts/v1/leptons/store.sol
+++ contracts/v1/leptons/store.sol
@@ -1,4 +1,5 @@
 pragma solidity 0.6.12;
+
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -12,77 +13,91 @@ interface ILepsonsStore {
 }
 
 contract LeptonsStore is ILepsonsStore, IERC721Receiver, Ownable, BlackholePrevention {
-    using SafeMath for uint256;
+  using SafeMath for uint256;
 
-    Lepton2 public lepton;
-    Ionx public ionx;
+  event SoldLepton(address indexed buyer, uint256 amount, uint256 price);
 
-    uint256 ionxPerLepton;
-    uint256 nextTtokenId;
+  Lepton2 public lepton;
+  Ionx public ionx;
 
-    event SoldLepton(address indexed buyer, uint256 amount, uint256 price);
+  uint256 internal ionxPerLepton;
+  uint256 internal nextTokenId;
 
-    constructor(address _lepton, address _ionx, uint256 _ionxPerLepton) public {
-        lepton = Lepton2(_lepton);
-        ionx = Ionx(_ionx);
-        ionxPerLepton = _ionxPerLepton;
+  constructor(address _lepton, address _ionx, uint256 _ionxPerLepton) public {
+    lepton = Lepton2(_lepton);
+    ionx = Ionx(_ionx);
+    ionxPerLepton = _ionxPerLepton;
+  }
+
+  function buyWithIonx(
+    uint256 leptonAmount,
+    uint256 deadline,
+    uint8 v,
+    bytes32 r,
+    bytes32 s
+  ) external {
+    uint256 ionxAmount = leptonAmount * ionxPerLepton;
+    require(ionx.balanceOf(msg.sender) >= ionxAmount, "Insufficient IONX balance");
+
+    ionx.permit(msg.sender, address(this), ionxAmount, deadline, v, r, s);
+    ionx.transferFrom(msg.sender, address(this), ionxAmount);
+
+    for (uint256 i = 0; i < leptonAmount; ++i) {
+      uint256 tokenId = nextTokenId;
+      nextTokenId = nextTokenId.add(1);
+
+      lepton.safeTransferFrom(address(this), msg.sender, tokenId);
     }
 
-    function load(uint256 amount) external payable override onlyOwner {
-        nextTtokenId =  lepton.totalSupply().add(1);
-        lepton.batchMintLepton{ value: msg.value }(amount);
-    }
+    emit SoldLepton(msg.sender, leptonAmount, ionxAmount);
+  }
 
-    function onERC721Received(address, address, uint256, bytes calldata) external override returns (bytes4) {
-        return IERC721Receiver.onERC721Received.selector;
-    }
+  function onERC721Received(address, address, uint256, bytes calldata) external override returns (bytes4) {
+    return IERC721Receiver.onERC721Received.selector;
+  }
 
-    function setLepton(address _lepton) external override onlyOwner {
-        lepton = Lepton2(_lepton);
-    }
+  /***********************************|
+  |          Only Admin/DAO           |
+  |__________________________________*/
 
-    function setIonx(address _ionx) external onlyOwner {
-        require(_ionx != address(0), "Invalid address");
-        ionx = Ionx(_ionx);
-    }
+  function load(uint256 amount) external payable override onlyOwner {
+    require(lepton.balanceOf(address(this)) == 0, "store not empty");
+    nextTokenId =  lepton.totalSupply().add(1);
+    lepton.batchMintLepton{ value: msg.value }(amount);
+  }
 
-    function setIonxPerLepton(uint256 ionxAmount) external onlyOwner {
-        ionxPerLepton = ionxAmount;
-    }
+  function setLepton(address _lepton) external override onlyOwner {
+    require(_lepton != address(0), "Invalid address");
+    lepton = Lepton2(_lepton);
+  }
 
-    function buyWithIonx(
-        uint256 leptonAmount,
-        uint256 deadline,
-        uint8 v,
-        bytes32 r,
-        bytes32 s
-    ) external {
-      uint256 ionxAmount = leptonAmount * ionxPerLepton;
-      require(ionx.balanceOf(msg.sender) >= ionxAmount, "Insufficient IONX balance");
+  function setIonx(address _ionx) external onlyOwner {
+    require(_ionx != address(0), "Invalid address");
+    ionx = Ionx(_ionx);
+  }
 
-      ionx.permit(msg.sender, address(this), ionxAmount, deadline, v, r, s);
-      ionx.transferFrom(msg.sender, address(this), ionxAmount);
+  function setIonxPerLepton(uint256 ionxAmount) external onlyOwner {
+    ionxPerLepton = ionxAmount;
+  }
 
-      for (uint256 i = 0; i < leptonAmount; ++i) {
-        uint256 tokenId =  nextTtokenId;
-        nextTtokenId = nextTtokenId.add(1);
+  /***********************************|
+  |          Only Admin/DAO           |
+  |      (blackhole prevention)       |
+  |__________________________________*/
 
-        lepton.safeTransferFrom(address(this), msg.sender, tokenId);
-      }
+  function withdrawEther(address payable receiver, uint256 amount) external onlyOwner {
+    _withdrawEther(receiver, amount);
+  }
 
-      emit SoldLepton(msg.sender, leptonAmount, ionxAmount);
-    }
+  function withdrawErc20(address payable receiver, address tokenAddress, uint256 amount) external onlyOwner {
+    _withdrawERC20(receiver, tokenAddress, amount);
+  }
 
-    // Black hole prevention.
-    function withdrawEther(address payable receiver, uint256 amount) external onlyOwner {
-        _withdrawEther(receiver, amount);
-    }
+  function withdrawERC721(address payable receiver, address tokenAddress, uint256 tokenId) external onlyOwner {
+    _withdrawERC721(receiver, tokenAddress, tokenId);
+  }
 
-    function withdrawErc20(address payable receiver, address tokenAddress, uint256 amount) external onlyOwner {
-        _withdrawERC20(receiver, tokenAddress, amount);
-    }
-
-    function withdrawERC721(address payable receiver, address tokenAddress, uint256 tokenId) external onlyOwner {
-        _withdrawERC721(receiver, tokenAddress, tokenId);
-    }
+  function withdrawERC1155(address payable receiver, address tokenAddress, uint256 tokenId, uint256 amount) external onlyOwner {
+    _withdrawERC1155(receiver, tokenAddress, tokenId, amount);
+  }
 }
