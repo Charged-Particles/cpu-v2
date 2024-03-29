605cb4a9df8e91484857b629c4fe8476ea7e1d70
scammi <scammi@gmail.com>
2024-03-29 12:44:37 -0300

    stablish store deploy

--- deploy/LeptonsStore.ts
+++ deploy/LeptonsStore.ts
@@ -12,24 +12,18 @@ const LeptonStore: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
     let lepton: Lepton2 ;
 	let ionx: Ionx;
 
-	lepton = await ethers.getContract('Lepton2');
-	ionx = await ethers.getContract('Ionx');
-
-	// if (isTestnet()) {
-	// 	lepton = await ethers.getContract('Lepton2');
-	// 	ionx = await ethers.getContract('Ionx');
-	// } else {
-	// 	lepton = await ethers.getContractAt('Lepton2', '0x3Cd2410EAa9c2dCE50aF6CCAb72Dc93879a09c1F');
-	// 	ionx = await ethers.getContractAt('Ionx', '0x02D3A27Ac3f55d5D91Fb0f52759842696a864217');
-	// }
+	if (isTestnet()) {
+		lepton = await ethers.getContract('Lepton2');
+		ionx = await ethers.getContract('Ionx');
+	} else {
+		lepton = await ethers.getContractAt('Lepton2', '0x3Cd2410EAa9c2dCE50aF6CCAb72Dc93879a09c1F');
+		ionx = await ethers.getContractAt('Ionx', '0x02D3A27Ac3f55d5D91Fb0f52759842696a864217');
+	}
 	
     const leptonAddress = await lepton.getAddress();
 	const ionxAddress = await ionx.getAddress();
 	const leptonPrice = 1;
 
-	console.log(leptonAddress, ionxAddress, deployer);
-
-	console.log(leptonAddress, ionxAddress, leptonPrice);
 	await deploy('LeptonsStore', {
 		from: deployer,
 		args: [ leptonAddress, ionxAddress, leptonPrice ],
